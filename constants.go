1ac20ef6317289fdd97fc93ed913b1314486428a
viktor <vi.dave@yandex.ru>
2023-02-04 12:03:16 +0300

    [upd] Realise methods PbiFullDeals, PbiFullObjects, PbiPrices, Table

---
+++ constants.go
@@ -0,0 +1,10 @@
+package bnmap
+
+type Method string
+
+const (
+	PbiFullDeals   = "pbi-full-deals"
+	PbiFullObjects = "pbi-full-objects"
+	PbiPrices      = "pbi-prices"
+	Table          = "table"
+)
